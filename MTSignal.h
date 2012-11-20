2638aad62f2f085d7fc9843309b75457e4f54e6e
Matthew Clever <clever@cleversoap.com>
2012-11-20 23:08:27 +1000

    MTSignal is functionally ready. Slots can be connected to the signal and called with MINIMAL (awesome) type templating (again, awesome).

--- MTSignal.h
+++ MTSignal.h
@@ -1,8 +1,28 @@
 #ifndef __MTSIGNAL_H__
+/*
+* Copyright (c) 2012 Cleversoap
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*/
+
 #define __MTSIGNAL_H__
 
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
@@ -11,19 +31,29 @@ class MTSignal
 {
 	
 	public:
-		MTSignal()
+		MTSignal(){}
+
+		// Call the operator as you normally would with parameters
+		// as the type templating has already been done
+		// This will iterate over each slot and call it with the passed params.
+		void operator() (const T&... args)
 		{
+			for(int i = 0; i < _slots.size(); ++i)
+			{
+				_slots.at(i)(args...);
+			}
 		}
 
-		void operator() (T... args)
+		// Connect a slot function that returns void and matches the
+		// signal delegate.
+		void connect(void (*slf)(T...))
 		{
-			for(int i = 0; i < slots.size(); ++i)
-			{
-				slots.at(i)(args...);
-			}
-		}	
+			_slots.push_back(slf);
+		}
 
-		vector<void (*) (T...)> slots;
+	protected:
+		// Vector of all slot functions
+		vector<void (*) (T...)> _slots;
 };
 
 #endif
